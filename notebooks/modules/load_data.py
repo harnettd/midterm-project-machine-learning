c041f1a238499bb5be8762d46a66991140722e42
Derek Harnett <derek.harnett@shaw.ca>
2024-05-18 10:49:46 -0700

    add script to read all raw json files

---
+++ notebooks/modules/load_data.py
@@ -0,0 +1,69 @@
+"""
+Load the raw data.
+"""
+import json
+import re 
+import pandas as pd
+
+from os import listdir
+
+
+def get_json_basenames(basenames: list[str]) -> list[str]:
+    """
+    Return the elements of basenames that end with '.json'.
+
+    :param basenames: A list of basenames
+    :type basenames: list[str]
+
+    :return: The elements of basenames that end with '.json'
+    :rtype: list[str]
+    """
+    json_basenames = []
+    for basename in basenames:
+        if re.fullmatch(r'.*\.json', basename) is not None:
+            json_basenames.append(basename)
+    return json_basenames
+
+
+def load_jsons(json_basenames: list[str], dirname: str) -> list:
+    """
+    Return a list of json objects.
+
+    :param json_basenames: A list of basenames each ending with '.json'
+    :type json_basename: list[str]
+
+    :param dirname: The name of the directory containing the basenames
+    :type dirname: str
+
+    :return: A list of json objects, each loaded from a basename
+    :rtype: list
+    """
+    jsons = []
+    for basename in json_basenames:
+        filename = dirname + basename
+        try:
+            with open(filename, 'r') as f:
+                json_obj = json.load(f)
+        except FileNotFoundError:
+            print(f'FileNotFoundError: {filename} does not exist')
+        except json.JSONDecodeError:
+            print(f'JSONDecodeError: {filename} does not contain valid JSON data')
+        else:
+            jsons.append(json_obj)
+    return jsons
+
+
+def load_data(dirname) -> pd.DataFrame:
+    """
+    Meh.
+    """ 
+    basenames = listdir(path=dirname)
+    json_basenames = get_json_basenames(basenames)    
+    jsons = load_jsons(json_basenames, dirname)
+    
+    return jsons
+    
+
+if __name__ == '__main__':
+    # print(__doc__)
+    data = load_data('data/raw/')
