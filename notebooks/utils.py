478998429f0ac510373d2313564441c9564a3f50
Derek Harnett <derek.harnett@shaw.ca>
2024-05-23 10:21:58 -0700

    ad adjusted r-squared

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -6,6 +6,18 @@ from numpy import ndarray, sqrt
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 
+def adj_r2_score(
+    X: ndarray, 
+    y_true: ndarray, 
+    y_pred: ndarray) -> float:
+    """
+    Return the adjusted R-squared score.
+    """
+    r_squared = r2_score(y_true, y_pred)
+    n, p = X.shape
+    return 1 - (n - 1) / (n - p - 1) * (1 - r_squared)
+
+
 def print_scores(
     train: list[ndarray, ndarray, ndarray],
     test: list[ndarray, ndarray, ndarray]
@@ -24,10 +36,12 @@ def print_scores(
     
     print(f'RMSE train: {sqrt(mean_squared_error(y_train, y_train_pred))}')
     print(f'RMSE test: {sqrt(mean_squared_error(y_test, y_test_pred))}')
-    print(f'MAE train: {sqrt(mean_absolute_error(y_train, y_train_pred))}')
-    print(f'MAE test: {sqrt(mean_absolute_error(y_test, y_test_pred))}')
-    print(f'R**2 train: {sqrt(r2_score(y_train, y_train_pred))}')
-    print(f'R**2 test: {sqrt(r2_score(y_test, y_test_pred))}')
+    print(f'MAE train: {mean_absolute_error(y_train, y_train_pred)}')
+    print(f'MAE test: {mean_absolute_error(y_test, y_test_pred)}')
+    print(f'R**2 train: {r2_score(y_train, y_train_pred)}')
+    print(f'R**2 test: {r2_score(y_test, y_test_pred)}')
+    print(f'Adj R**2 train: {adj_r2_score(X_train, y_train, y_train_pred)}')
+    print(f'Adj R**2 test: {adj_r2_score(X_test, y_test, y_test_pred)}')
 
 
 def make_coef_dict(X: pd.DataFrame, coefs: ndarray) -> dict:
