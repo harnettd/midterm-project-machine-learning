11f1e605d1b45a4b0c962856b1f7af99ad822f3c
Derek Harnett <derek.harnett@shaw.ca>
2024-05-23 10:05:58 -0700

    update print_scores function

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,5 +1,5 @@
 """
-Some utilitey functions.
+Some utility functions.
 """
 import pandas as pd
 from numpy import ndarray, sqrt
@@ -7,14 +7,21 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 
 def print_scores(
-        y_train,
-        y_train_pred,
-        y_test, 
-        y_test_pred
+    train: list[ndarray, ndarray, ndarray],
+    test: list[ndarray, ndarray, ndarray]
 ) -> None:
     """
     Print various machine-learning regression test scores.
+
+    :param train: Training data, *i.e.,* [X_train, y_train, y_train_pred]
+    :type train: list[ndarray, ndarray]
+
+    :param test: Testing data, *i.e.,* [X_test, y_test, y_test_pred]
+    :type test: list[ndarray, ndarray]
     """
+    X_train, y_train, y_train_pred = train
+    X_test, y_test, y_test_pred = test
+    
     print(f'RMSE train: {sqrt(mean_squared_error(y_train, y_train_pred))}')
     print(f'RMSE test: {sqrt(mean_squared_error(y_test, y_test_pred))}')
     print(f'MAE train: {sqrt(mean_absolute_error(y_train, y_train_pred))}')
